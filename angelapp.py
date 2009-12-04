da8489785c6525aaffdf48cc7368b0e0b9adb787
toomore <toomore0929@gmail.com>
2009-12-04 18:01:54 +0800

    Change mailbox to mune app.

--- angelapp.py
+++ angelapp.py
@@ -32,8 +32,12 @@ class angelmenu:
       a = '哭哭！沒有小天使！'
     return a
 
+  def showmailboxmenu(self):
+    a = '<a href="/mailbox">Mailbox</a>'
+    return a
+
   def listmenu(self):
-    a = self.mastermenu() + " | " + self.myangel()
+    a = self.showmailboxmenu + " | " + self.mastermenu() + " | " + self.myangel()
     return a
 
 class sendmails(angelmenu):
