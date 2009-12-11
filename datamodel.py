c43219c84b44d04ab548a25a4f41158381badd92
toomore <toomore0929@gmail.com>
2009-12-12 01:38:06 +0800

    Delete some test codes.

--- datamodel.py
+++ datamodel.py
@@ -21,7 +21,6 @@ class angeldata(db.Model):
   """ User data. """
   mymaster = db.EmailProperty()
   nickname = db.StringProperty()
-  #refs = db.ReferenceProperty(angelmasterlist,collection_name = 'emails')
   created_at = db.DateTimeProperty(auto_now_add = True)
 
 class angelmailbox(db.Model):
